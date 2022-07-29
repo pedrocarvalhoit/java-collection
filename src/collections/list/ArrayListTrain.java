0b2f52950f851031916af478ec05044aead7f210
Pedro Duarte <pedrohdc@yahoo.com.br>
2022-07-29 14:33:16 +0100

    Adicionando projeto ao git hub

---
+++ src/collections/list/ArrayListTrain.java
@@ -0,0 +1,40 @@
+package collections.list;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class ArrayListTrain {
+	public static void main(String[] args) {
+		
+		List<String> myList = new ArrayList<>();
+		myList.add("Object 1");
+		myList.add("Object 2");
+		myList.add("Object 3");
+		
+		//	myList.remove(2);
+		
+		//Cast needed to creat a variable when the List has no String specification
+		String firstObjecti = (String) myList.get(0);
+		
+		//Print Array
+		Iterator iterator = myList.iterator();
+		while(iterator.hasNext()){
+			System.out.println(iterator.next());
+		}
+		
+		System.out.println("-- foreach--");
+		for (Object next : myList) {
+			System.out.println(next);
+		}
+		
+		System.out.println("-- Lambda--");
+		myList.forEach(next -> {
+			System.out.println(next);
+		});
+		
+		myList.clear();
+		
+	}
+
+}
---
+++ src/collections/list/LinkedListTrain.java
@@ -0,0 +1,5 @@
+package collections.list;
+
+public class LinkedListTrain {
+
+}
